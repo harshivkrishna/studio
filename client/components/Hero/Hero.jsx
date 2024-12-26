34e8e417be3b38f4c95d62f6a8db39bc50ae11a9
harshivkrishna <harshivkrishnam.cse2023@citchennai.net>
2024-12-26 20:24:41 +0530

    added image fetching from server

--- client/components/Hero/Hero.jsx
+++ client/components/Hero/Hero.jsx
@@ -1,74 +1,162 @@
-"use client"
-import React, { useEffect } from 'react'
-import './Hero.css'
-import { Navigation, Pagination, A11y,Autoplay } from 'swiper/modules';
-import 'swiper/css/autoplay'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import ScrollReveal from 'scrollreveal';
-import { Link } from 'react-router-dom';
+"use client";
+import React, { useEffect, useRef, useState } from 'react';
+import './Hero.css';
 import 'boxicons/css/boxicons.min.css';
+
 const Hero = () => {
-  // useEffect(() => {
-  //   // Initialize ScrollReveal
-  //   ScrollReveal().reveal('.heading, .para', {
-  //     origin:"top",
-  //     distance: '50px',
-  //     duration: 1000,
-  //     delay: 200,
-  //     opacity: 0,
-  //     interval: 100,
-  //   });
-  // }, []);
+  const [runNextAuto, setRunNextAuto] = useState(null);
+  const nextDom = useRef(null);
+  const prevDom = useRef(null);
+  const carouselDom = useRef(null);
+  const sliderDom = useRef(null);
+  const thumbnailBorderDom = useRef(null);
+  const thumbnailItemsDom = useRef([]);
+  const timeDom = useRef(null);
+  const timeRunning = 3000;
+  const timeAutoNext = 7000;
+
+  const showSlider = (type) => {
+    const sliderItemsDom = sliderDom.current.querySelectorAll('.carousel .list .item');
+    const thumbnailItems = thumbnailBorderDom.current.querySelectorAll('.carousel .thumbnail .item');
+
+    if (type === 'next') {
+      sliderDom.current.appendChild(sliderItemsDom[0]);
+      thumbnailBorderDom.current.appendChild(thumbnailItems[0]);
+      carouselDom.current.classList.add('next');
+    } else {
+      sliderDom.current.prepend(sliderItemsDom[sliderItemsDom.length - 1]);
+      thumbnailBorderDom.current.prepend(thumbnailItems[thumbnailItems.length - 1]);
+      carouselDom.current.classList.add('prev');
+    }
+
+    clearTimeout(runNextAuto);
+    setRunNextAuto(
+      setTimeout(() => {
+        nextDom.current.click();
+      }, timeAutoNext)
+    );
+
+    setTimeout(() => {
+      carouselDom.current.classList.remove('next');
+      carouselDom.current.classList.remove('prev');
+    }, timeRunning);
+  };
+
+  useEffect(() => {
+    const nextButton = nextDom.current;
+    const prevButton = prevDom.current;
+
+    nextButton.onclick = () => {
+      showSlider('next');
+    };
+
+    prevButton.onclick = () => {
+      showSlider('prev');
+    };
+
+    const autoNext = setTimeout(() => {
+      nextButton.click();
+    }, timeAutoNext);
+
+    // Cleanup on component unmount
+    return () => {
+      clearTimeout(autoNext);
+      clearTimeout(runNextAuto);
+    };
+  }, [runNextAuto]);
+
   return (
-    <div className='hero'>
-    <Swiper
-      modules={[Navigation, Pagination, A11y,Autoplay]}
-      spaceBetween={50}
-      slidesPerView={1}
-      // autoplay={{ delay: 4000}}
-      navigation
-      loop={true}
-      pagination={{ clickable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide className='slide'>
-          <div className='slide-content'>
-          <img src="/assests/slider/img1.jpg" alt="" />
-          <div>
-            <p>Welcome to Mighty Plethora</p>
-            <h1 className='heading'>Transform Your Photos, Transform Your World</h1>
-            <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae reiciendis at mollitia minima est aspernatur perferendis praesentium ad autem!</p>
-            <div className='button-container'>
-            <a href='/' className='explore-more'>Explore More</a>
-            <a href='/' className='play-icon'><i className='bx bxs-right-arrow'></i></a>
-            <p>Play Video</p>
+    <div className="carousel" ref={carouselDom}>
+      {/* list item */}
+      <div className="list" ref={sliderDom}>
+        <div className="item">
+          <img src="/assests/slider/img1.jpg" alt="Slide 1" />
+          <div className="content">
+            <div className="author">Welcome to Mighty Plethora</div>
+            <div className="title">DESIGN SLIDER</div>
+            {/* <div className="topic">ANIMAL</div> */}
+            <div className="des">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,
+              rem magnam nesciunt minima placeat, itaque eum neque officiis unde
+              
             </div>
+            <div className="buttons">
+              <button>SEE MORE</button>
+              {/* <button>SUBSCRIBE</button> */}
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <img src="/assests/slider/img2.jpg" alt="Slide 2" />
+          <div className="content">
+            <div className="author">Welcome to Mighty Plethora</div>
+            <div className="title"></div>
+            {/* <div className="topic">ANIMAL</div> */}
+            <div className="des">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,
+              rem magnam nesciunt minima placeat, itaque eum neque officiis unde,
+              
+            </div>
+            <div className="buttons">
+              <button>SEE MORE</button>
+              {/* <button>SUBSCRIBE</button> */}
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <img src="/assests/slider/img3.jpg" alt="Slide 3" />
+          <div className="content">
+            <div className="author">Welcome to Mighty Plethora</div>
+            <div className="title">DESIGN SLIDER</div>
+            {/* <div className="topic">ANIMAL</div> */}
+            <div className="des">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi,
+              rem magnam nesciunt minima placeat, itaque eum neque officiis unde,
+              
+            </div>
+            <div className="buttons">
+              <button>SEE MORE</button>
+              {/* <button>SUBSCRIBE</button> */}
+            </div>
+          </div>
+        </div>
+      </div>
 
+      {/* list thumbnail */}
+      <div className="thumbnail" ref={thumbnailBorderDom}>
+        <div className="item">
+          <img src="/assests/slider/img1.jpg" alt="Thumbnail 1" />
+          <div className="content">
+            <div className="title">Name Slider</div>
+            <div className="description">Description</div>
           </div>
+        </div>
+        <div className="item">
+          <img src="/assests/slider/img2.jpg" alt="Thumbnail 2" />
+          <div className="content">
+            <div className="title">Name Slider</div>
+            <div className="description">Description</div>
           </div>
-        </SwiperSlide>
-      <SwiperSlide className='slide'>
-          <div className='slide-content'>
-          <img src="/assests/slider/img2.jpg" alt="" />
-          <h1 className='heading'>hello world</h1>
-          <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae reiciendis at mollitia minima est aspernatur perferendis praesentium ad autem!</p>
+        </div>
+        <div className="item">
+          <img src="/assests/slider/img3.jpg" alt="Thumbnail 3" />
+          <div className="content">
+            <div className="title">Name Slider</div>
+            <div className="description">Description</div>
           </div>
-      </SwiperSlide>
-      <SwiperSlide className='slide'>
-      <div className='slide-content'>
-          <img src="/assests/slider/img3.jpg" alt="" />
-          <h1 className='heading'>hello world</h1>
-          <p className='para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum vitae reiciendis at mollitia minima est aspernatur perferendis praesentium ad autem!</p>
-          </div>
-        </SwiperSlide>
-    </Swiper>
+        </div>
+      </div>
+
+      {/* next prev */}
+      <div className="arrows">
+        <button ref={prevDom} id="prev">&lt;</button>
+        <button ref={nextDom} id="next">&gt;</button>
+      </div>
+
+      {/* time running */}
+      <div className="time" ref={timeDom}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- client/components/Navbar/Navbar.jsx
+++ client/components/Navbar/Navbar.jsx
@@ -1,20 +1,58 @@
-"use client"
-import React from 'react'
-import './Navbar.css'
+"use client";
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-  return (
-    <nav className='navbar'>
-        <img src="/assests/logo.png" alt="" />
-        <ul>
-                <li><a href={'/'}>Home</a></li>
-                <li><a href={'/about'}>About</a></li>
-                <li><a href={'/about'}>Projects</a></li>          
-                <li><a href={'/contact'}>Contact</a></li>
-                <li><a href={'/careers'}>Careers</a></li>
-        </ul>
-    </nav>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
+  const [bg, setBg] = useState(false);
 
-export default Navbar
+  // Lock body scroll when sidebar is open
+  useEffect(() => {
+    document.body.style.overflow = isOpen ? "hidden" : "visible";
+  }, [isOpen]);
+
+  // Add or remove `bg` class on scroll
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 400) {
+        setBg(true);
+      } else {
+        setBg(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav className={`navbar ${bg ? "bg" : ""}`}>
+      <img src="/assests/logo.png" alt="Logo" />
+      <ul className="menu-links">
+        <li><a href={"/"}>Home</a></li>
+        <li><a href={"/about"}>About</a></li>
+        <li><a href={"/contact"}>Contact</a></li>
+        <li><a href={"/careers"}>Careers</a></li>
+      </ul>
+      <i className="bx bx-menu menu-icon" onClick={() => setIsOpen(true)}></i>
+      <div className={`sidebar ${isOpen ? "open" : ""}`}>
+        <div className="sidebar-top">
+          <img src="/assests/logo.png" alt="Logo" />
+          <i className="bx bx-x close-icon" onClick={() => setIsOpen(false)}></i>
+        </div>
+        <ul className="sidebar-links">
+          <li><a href={"/"}>Home</a></li>
+          <li><a href={"/about"}>About</a></li>
+          <li><a href={"/contact"}>Contact</a></li>
+          <li><a href={"/careers"}>Careers</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
